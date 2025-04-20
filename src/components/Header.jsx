a845cc9cd4770ba921e81a3350bb06162417bdb2
mohamedhanafi69120 <your-email@example.com>
2025-04-20 20:11:59 +0200

    projet fianliser

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from "../images/argentBankLogo.webp";
-// import houseIcon from "../images/house.svg";
-
 import { logout } from "../redux/userSlice";
+import houseIcon from "../images/house.svg";
 
 const Header = () => {
   const location = useLocation();
+  const isHomePage = location.pathname === "/";
   const isSignInPage = location.pathname === "/sign-in";
-  const isSignIn = useSelector((state) => state.user.token);
-  const userName = useSelector((state) => state.user.userName);
+  const isSignIn = !!useSelector((state) => state.user.token);
+  const { userName } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -21,13 +21,13 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
+    <header>
       <nav className="main-nav">
         <Link className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
             src={logo}
-            alt="argentbank logo"
+            alt="ArgentBank logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
         </Link>
@@ -35,13 +35,21 @@ const Header = () => {
         {isSignIn ? (
           <div>
             <Link to="/user" className="main-nav-item">
-              <i className="fa fa-user-circle"></i> {userName}
+              <i className="fa fa-user-circle"></i>
+              {userName}
             </Link>
             <button className="main-nav-item" onClick={signOut}>
-              <i className="fa fa-sign-out"></i> Sign Out
+              <i className="fa fa-sign-out"></i>
+              Sign Out
             </button>
           </div>
-        ) : isSignInPage ? null : (
+        ) : isSignInPage ? (
+          // ✅ On affiche "Home" (icône maison) seulement si on est sur /sign-in
+          <Link to="/" className="main-nav-item house">
+            <img src={houseIcon} alt="Home" />
+            Home
+          </Link>
+        ) : (
           <Link to="/sign-in" className="main-nav-item">
             <i className="fa fa-user-circle"></i> Sign In
           </Link>
